423b7a607df6da79876f7b1c9f67aac0558a4cb0
msjun23 <msjun23@gmail.com>
2024-05-15 13:57:27 +0900

    Debugging metric code

--- scripts/models/stereo_depth_model.py
+++ scripts/models/stereo_depth_model.py
@@ -28,10 +28,10 @@ class StereoDepthLightningModule(pl.LightningModule):
         
         self.dataset = dataset
         
-        # Train, validation and test step outputs buffer
-        self.training_step_outputs = []
-        self.validation_step_outputs = []
-        self.test_step_outputs = []
+        # # Train, validation and test step outputs buffer
+        # self.training_step_outputs = []
+        # self.validation_step_outputs = []
+        # self.test_step_outputs = []
         
     def forward(self, stereo_event, stereo_image):
         rose_img = {}
@@ -67,69 +67,73 @@ class StereoDepthLightningModule(pl.LightningModule):
         stereo_event = batch['event'] if 'event' in batch else None     # ['left', 'right'], [N, C, H, W]
         stereo_image = batch['image'] if 'image' in batch else None     # ['left', 'right'], [N, C, H, W]
         disparity_gt = batch['disparity_gt'] if 'disparity_gt' in batch else None   # [N, H, W]
-        file_index = batch['file_index'] if 'file_index' in batch else None
+        file_index = batch['file_index'] if 'file_index' in batch else None         # List, length=batch_size
         # seq_name = batch['seq_name'] if 'seq_name' in batch else None
+        batch_size = len(file_index)
         
         # Forward pass
         start_t = time.time()
         pred_disparity_pyramid = self.forward(stereo_event, stereo_image)   # List: [[N, H, W], ...]
         fps = 1 / ((time.time() - start_t) / pred_disparity_pyramid[-1].shape[0])
+        self.log('fps', fps, prog_bar=True, on_step=True)           # FPS
         
         # Calculate loss
         loss_disp = self._cal_loss(pred_disparity_pyramid, disparity_gt)
         loss = loss_disp.mean()
-        self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size, sync_dist=True)
         
         # Calculate metrics
         pred = pred_disparity_pyramid[-1].detach()
         gt   = disparity_gt.detach()
-        num_imgs = len(file_index)
         mask = (gt > 0) & (gt < self.max_disp)
-        step_mde = mean_depth_error(pred, gt, mask, on_epoch=False)
-        step_mdise = mean_disparity_error(pred, gt, mask, on_epoch=False)
-        step_1pa = n_pixel_accuracy(pred, gt, mask, 1, on_epoch=False)
         
-        # Logging
-        cur_lr = self.trainer.optimizers[0].param_groups[0]['lr']
-        self.log('lr', cur_lr, prog_bar=True, on_step=True)         # Learning rate
-        self.log('fps', fps, prog_bar=True, on_step=True)           # FPS
+            # MVSEC
+        step_mde = step_mdise = step_1pa = 0.
+        for p, g, m in zip(pred, gt, mask):
+            p, g = p[m], g[m]
+            step_mde += mean_depth_error(p, g)
+            step_mdise += mean_disparity_error(p, g)
+            step_1pa += n_pixel_accuracy(p, g, 1)
+        step_mde, step_mdise, step_1pa = step_mde/batch_size, step_mdise/batch_size, step_1pa/batch_size
+        self.log('train_mde', step_mde, on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size, sync_dist=True)
+        self.log('train_mdise', step_mdise, on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size, sync_dist=True)
+        self.log('train_1pa', step_1pa, on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size, sync_dist=True)
         
         # Training step outputs
-        output_dict = {'loss': loss, 'FPS': fps, 'MDE': step_mde, 'MDisE': step_mdise, '1PA': step_1pa, 'num_imgs': num_imgs}
-        self.training_step_outputs.append(output_dict)
-        return output_dict
+        # output_dict = {'MDE': step_mde, 'MDisE': step_mdise, '1PA': step_1pa, 'num_imgs': num_imgs}
+        # self.training_step_outputs.append(output_dict)
+        return loss
     
-    def on_train_epoch_end(self):
-        # Gather all batch metrics
-        num_imgs = 0
-        epoch_mde = 0.
-        epoch_mdise = 0.
-        epoch_1pa = 0.
-        for batch_output in self.training_step_outputs:
-            num_imgs += batch_output['num_imgs']
-            epoch_mde += batch_output['MDE']
-            epoch_mdise += batch_output['MDisE']
-            epoch_1pa += batch_output['1PA']
+    # def on_train_epoch_end(self):
+    #     # Gather all batch metrics
+    #     num_imgs = 0
+    #     epoch_mde = epoch_mdise = epoch_1pa = 0.
+    #     for batch_output in self.training_step_outputs:     # for dict in list
+    #         num_imgs += batch_output['num_imgs']
+    #         epoch_mde += batch_output['MDE']
+    #         epoch_mdise += batch_output['MDisE']
+    #         epoch_1pa += batch_output['1PA']
         
-        # Metrics on an epoch
-        epoch_mde /= num_imgs
-        epoch_mdise /= num_imgs
-        epoch_1pa /= num_imgs
+    #     # Metrics on an epoch
+    #     epoch_mde /= num_imgs
+    #     epoch_mdise /= num_imgs
+    #     epoch_1pa /= num_imgs
         
-        # Log epoch metrics
-        self.log('MDE', epoch_mde, prog_bar=True, on_epoch=True)
-        self.log('MDisE', epoch_mdise, prog_bar=True, on_epoch=True)
-        self.log('1PA', epoch_1pa, prog_bar=True, on_epoch=True)
+    #     # Log epoch metrics
+    #     self.log('MDE', epoch_mde, prog_bar=True, on_epoch=True, sync_dist=True)
+    #     self.log('MDisE', epoch_mdise, prog_bar=True, on_epoch=True, sync_dist=True)
+    #     self.log('1PA', epoch_1pa, prog_bar=True, on_epoch=True, sync_dist=True)
         
-        # Free up the memory
-        self.training_step_outputs.clear()
+    #     # Free up the memory
+    #     self.training_step_outputs.clear()
     
     def validation_step(self, batch, batch_idx):
         stereo_event = batch['event'] if 'event' in batch else None     # ['left', 'right'], [N, C, H, W]
         stereo_image = batch['image'] if 'image' in batch else None     # ['left', 'right'], [N, C, H, W]
         disparity_gt = batch['disparity_gt'] if 'disparity_gt' in batch else None   # [N, H, W]
-        # file_index = batch['file_index'] if 'file_index' in batch else None
+        file_index = batch['file_index'] if 'file_index' in batch else None
         # seq_name = batch['seq_name'] if 'seq_name' in batch else None
+        batch_size = len(file_index)
         
         # Forward pass
         pred_disparity_pyramid = self.forward(stereo_event, stereo_image)
@@ -139,7 +143,7 @@ class StereoDepthLightningModule(pl.LightningModule):
         if disparity_gt is not None:
             loss_disp = self._cal_loss(pred_disparity_pyramid, disparity_gt)
             loss = loss_disp.mean()
-            self.log('val_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+            self.log('val_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size, sync_dist=True)
         return loss
     
     def configure_optimizers(self):
--- scripts/utils/metrics.py
+++ scripts/utils/metrics.py
@@ -9,68 +9,30 @@ def disparity_to_depth(disparity_map):
     depth_map = FOCAL_LENGTH_X_BASELINE['indoor_flying'] / (disparity_map + 1e-7)
     return depth_map
 
-def error_per_image(_pred, _gt, _mask):
-    assert _pred.shape == _gt.shape == _mask.shape
-    _pred, _gt = _pred[_mask], _gt[_mask]
-    _error = torch.abs(_pred - _gt)
-    return _error.mean()
-
-def mean_depth_error(pred, gt, mask=None, on_epoch=True):
-    # pred, gt, mask: (N, H, W), Channel of disparity map is one
-    assert pred.shape == gt.shape == mask.shape
+def mean_depth_error(pred, gt):
+    # pred, gt, mask: (H, W)
+    assert pred.shape == gt.shape    
+    mask = pred > 0.
+    pred, gt = pred[mask], gt[mask]
+    assert pred.shape == gt.shape
     
-    error = 0.
-    for _pred, _gt, _mask in zip(pred, gt, mask):
-        # Disparity to depth
-        _pred = disparity_to_depth(_pred)
-        _gt = disparity_to_depth(_gt)
-        error += error_per_image(_pred, _gt, _mask)
-        
-    if on_epoch:
-        # Returns final average error for all batches
-        error = error.mean()
-    else:   # on step
-        # Returns the sum of errors for the current batch
-        # Final average error should be calculated outside this function
-        error = error.sum()
-    return error
+    pred = disparity_to_depth(pred)
+    gt = disparity_to_depth(gt)
+    error = torch.abs(pred - gt)
+    return error.mean() * 100       # report in [cm]
 
-def mean_disparity_error(pred, gt, mask=None, on_epoch=True):
+def mean_disparity_error(pred, gt):
     # This is same to mean_average_error
-    # pred, gt, mask: (N, H, W), Channel of disparity map is one
-    assert pred.shape == gt.shape == mask.shape
+    # pred, gt, mask: (H, W)
+    assert pred.shape == gt.shape
     
-    error = 0.
-    for _pred, _gt, _mask in zip(pred, gt, mask):
-        error += error_per_image(_pred, _gt, _mask)
-    
-    if on_epoch:
-        # Returns final average error for all batches
-        error = error.mean()
-    else:   # on step
-        # Returns the sum of errors for the current batch
-        # Final average error should be calculated outside this function
-        error = error.sum()
-    return error
+    error = torch.abs(pred - gt)
+    return error.mean()             # report in [pix]
 
-def n_pixel_accuracy(pred, gt, mask=None, n=1, on_epoch=True):
-    # This is same to mean_average_error
-    # pred, gt, mask: (N, H, W), Channel of disparity map is one
-    assert pred.shape == gt.shape == mask.shape
+def n_pixel_accuracy(pred, gt, n=1):
+    # pred, gt, mask: (H, W)
+    assert pred.shape == gt.shape
     
-    n_pa = 0.
-    for _pred, _gt, _mask in zip(pred, gt, mask):
-        _pred, _gt = _pred[_mask], _gt[_mask]
-        _error = torch.abs(_pred - _gt)
-        _error_mask = _error <= n
-        _error_mask = _error_mask.to(torch.float)
-        n_pa += _error_mask.mean()       # n pixel accuracy for an image
-        
-    if on_epoch:
-        # Returns final average error for all batches
-        n_pa = n_pa.mean()
-    else:   # on step
-        # Returns the sum of errors for the current batch
-        # Final average error should be calculated outside this function
-        n_pa = n_pa.sum()
-    return n_pa
+    error = torch.abs(pred - gt)
+    error_mask = (error <= n).to(torch.float)
+    return error_mask.mean() * 100  # report in [%]
