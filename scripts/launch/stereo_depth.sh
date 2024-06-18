8b89c63aa99bdff2328a065449057d4e4bce13ea
msjun23 <msjun23@gmail.com>
2024-06-18 16:08:34 +0900

    Update .sh file

--- scripts/launch/stereo_depth.sh
+++ scripts/launch/stereo_depth.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cfg_path=/root/code/configs/depth/mvsec.yaml
+cfg_path=/root/code/configs/depth/mvsec_s4.yaml
 save_dir=/root/code/save
 exp_name='test'
 
-python ../stereo_depth_main.py --cfg_path ${cfg_path} --save_dir ${save_dir}
+python ../stereo_depth_main.py --cfg_path ${cfg_path} --save_dir ${save_dir} --exp_name ${exp_name}
